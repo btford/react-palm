91bbf1785ed41f13b324300d15d0da4ae3640b25
Brian Ford <btford@umich.edu>
2016-08-26 22:47:38 -0700

    Fix: add missing enzyme typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="modules/enzyme/index.d.ts" />
 /// <reference path="modules/react-dom/index.d.ts" />
 /// <reference path="modules/react-redux/index.d.ts" />
 /// <reference path="modules/react/index.d.ts" />
