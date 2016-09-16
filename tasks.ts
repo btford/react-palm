5fb853aa8bf088d41427d44aaef23323a791b6d8
Brian Ford <btford@umich.edu>
2016-09-16 14:18:50 -0700

    Fix: show correct misuse of task API

--- tasks.ts
+++ tasks.ts
@@ -78,6 +78,7 @@ export const taskMiddleware = store => next => action => {
     taskHandlers.forEach((tasks, handler) => handler(tasks, dispatch));
 
     tasks = [];
+    lastWithTaskCall = null;
   }
 
   return returnValue;
