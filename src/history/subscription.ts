0c6b9c08ddb652d2bf46239358950a54b98dd9ad
Balthazar Gronon <balthazar@uber.com>
2017-01-06 15:13:25 -0800

    Fix: pass the path in the listener

--- src/history/subscription.ts
+++ src/history/subscription.ts
@@ -10,9 +10,10 @@ const MAKE_ASYNC_PROMISE = Promise.resolve(null);
 
 makeSubscriptionType(HISTORY_SUBSCRIPTION, (dispatch, added, removed) => {
   added.forEach((subscription) => {
-    const listener = (event) => dispatch(subscription.action(event));
-    const unlisten = getHistory().listen((event) => {
-      return MAKE_ASYNC_PROMISE.then(() => listener(event));
+    const listener = event => dispatch(subscription.action(event));
+    const unlisten = getHistory().listen(event => {
+      return MAKE_ASYNC_PROMISE
+        .then(() => listener(`${event.pathname}${event.search}${event.hash}`));
     });
 
     subToListener.set(subscription, unlisten);
